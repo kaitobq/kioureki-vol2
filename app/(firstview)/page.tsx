c81899f7f40d8eb4b3a84f62e8320c63ebfeff72
kaitobq <kaitobq520@gmail.com>
2024-03-19 21:03:47 +0900

    firstviewページ仮デザイン

--- app/(firstview)/page.tsx
+++ app/(firstview)/page.tsx
@@ -9,10 +9,13 @@ export default function Home() {
       <Box
         height="90vh"
         display="flex"
+        flexDirection="column"
         alignItems="center"
         justifyContent="center"
       >
-        <Text>Hello</Text>
+        <Text fontWeight="bold" fontSize={20}>
+          既往歴を管理できるWebアプリです
+        </Text>
         <>
           <Text>初めての方はこちら</Text>
           <Button
@@ -22,6 +25,17 @@ export default function Home() {
             sx={{ "&:hover": { color: "black" }, transition: "0.3s" }}
           >
             <Link href="/sign-up">Sign Up</Link>
+          </Button>
+        </>
+        <>
+          <Text>既に登録されている方はこちら</Text>
+          <Button
+            variant="outline"
+            bgColor="#6064df"
+            color="white"
+            sx={{ "&:hover": { color: "black" }, transition: "0.3s" }}
+          >
+            <Link href="/sign-in">Sign In</Link>
           </Button>
         </>
       </Box>
