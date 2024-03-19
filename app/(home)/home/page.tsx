8c9d6a0b5836d86b34aaa9e48ac3a61d9e3fdb7a
kaitobq <kaitobq520@gmail.com>
2024-03-19 20:54:15 +0900

    homeページ作成

--- app/(home)/home/page.tsx
+++ app/(home)/home/page.tsx
@@ -1,5 +1,39 @@
+"use client";
+
+import { OrganizationList, useOrganization } from "@clerk/nextjs";
+import { Box } from "@yamada-ui/react";
+
 const page = () => {
-  return <div>page</div>;
+  const { invitations } = useOrganization({
+    invitations: {
+      pageSize: 20,
+      initialPage: 2, // skips the first page
+    },
+  });
+  const { memberships } = useOrganization({
+    memberships: {
+      infinite: true, // Append new data to the existing list
+      keepPreviousData: true, // Persist the cached data until the new data has been fetched
+    },
+  });
+  if (!memberships) {
+    // Handle loading state
+    return null;
+  }
+  return (
+    <Box>
+      <OrganizationList hidePersonal />
+      <ul>
+        {memberships.data?.map((membership) => (
+          <li key={membership.id}>
+            {membership.publicUserData.firstName}{" "}
+            {membership.publicUserData.lastName} &lt;
+            {membership.publicUserData.identifier}&gt; :: {membership.role}
+          </li>
+        ))}
+      </ul>
+    </Box>
+  );
 };
 
 export default page;
