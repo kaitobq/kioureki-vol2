bf7bfe2b5384df32e675b6e35b38f07853c9cc99
kaitobq <kaitobq520@gmail.com>
2024-03-19 20:51:42 +0900

    organizationページ作成

---
+++ app/(dashoboard)/organization/[organizationid]/page.tsx
@@ -0,0 +1,11 @@
+import { Box, Text } from "@yamada-ui/react";
+
+const OrganizationIdPage = () => {
+  return (
+    <Box>
+      <Text>既往歴</Text>
+    </Box>
+  );
+};
+
+export default OrganizationIdPage;
