b607603835bd6abb305da8747b1585a9f237a8ac
kaitobq <kaitobq520@gmail.com>
2024-03-19 20:54:43 +0900

    firstviewページ作成

--- app/(firstview)/page.tsx
+++ app/(firstview)/page.tsx
@@ -1,15 +1,29 @@
-import { Box } from "@yamada-ui/react";
+"use client";
+
+import { Box, Button, Text } from "@yamada-ui/react";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <Box
-        height="100vh"
+        height="90vh"
         display="flex"
         alignItems="center"
         justifyContent="center"
       >
-        hello
+        <Text>Hello</Text>
+        <>
+          <Text>初めての方はこちら</Text>
+          <Button
+            variant="outline"
+            bgColor="#6064df"
+            color="white"
+            sx={{ "&:hover": { color: "black" }, transition: "0.3s" }}
+          >
+            <Link href="/sign-up">Sign Up</Link>
+          </Button>
+        </>
       </Box>
     </>
   );
