bddf877e5cdcb9733a22ed1b42d1bf5debf0f26e
kaitobq <kaitobq520@gmail.com>
2024-03-25 12:48:05 +0900

    addOrganization削除

--- lib/supabaseFunctions.ts
+++ lib/supabaseFunctions.ts
@@ -9,19 +9,3 @@ export const fetchDatabase = async (tableName: string) => {
     console.log(error);
   }
 };
-
-type organizationData = {
-  name: string;
-  members: JSON;
-};
-
-export const addOrganizationData = async ({
-  name,
-  members,
-}: organizationData) => {
-  try {
-    await supabase.from("Organizations").insert({ name, members });
-  } catch (error) {
-    console.log(error);
-  }
-};
